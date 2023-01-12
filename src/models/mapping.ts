40259cdab0c7406c53de0e45bf41b5296c015022
M0stafa123 <jodat.ahmed135@gmail.com>
2023-01-12 20:24:48 +0200

    complete

--- src/models/mapping.ts
+++ src/models/mapping.ts
@@ -11,7 +11,7 @@ export class mapping {
   async join(): Promise<fetching[]> {
     try {
       const conn = await client.connect();
-      const sql = 'SELECT name, price, order_id FROM products INNER JOIN mapping ON products.id=mapping.product_id;';
+      const sql = 'SELECT user_id,quantity,product_id,name FROM orders  INNER JOIN products ON orders.id = products.id';
       const result = await conn.query(sql);
       conn.release();
       return result.rows;
@@ -23,8 +23,7 @@ export class mapping {
   async top5(): Promise<fetching[]> {
     try {
       const conn = await client.connect();
-      const sql =
-        'SELECT name FROM products INNER JOIN mapping ON products.id=mapping.product_id ORDER BY quantity DESC LIMIT 5;';
+      const sql = 'SELECT name FROM orders  INNER JOIN products ON orders.id = products.id ORDER BY quantity DESC LIMIT 5;';
       const result = await conn.query(sql);
       conn.release();
       return result.rows;
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,6 @@ import MAPROUTE from './api/maaping_route';
 import ORDEROUTE from './api/order_route';
 import PRODUCTROUTE from './api/product_route';
 import USEROUTE from './api/user_route';
-import verifyToken from '../auth';
 const routes = express.Router();
 routes.use('/products', PRODUCTROUTE);
 routes.use('/users', USEROUTE);

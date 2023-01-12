a5b4b5194fb981ea522cb63ba6c578c56bb22c33
M0stafa123 <jodat.ahmed135@gmail.com>
2023-01-12 18:26:46 +0200

    .

--- src/models/mapping.ts
+++ src/models/mapping.ts
@@ -3,17 +3,16 @@ import client from '../database';
 //T refers to type
 export interface fetching {
   id?: number;
-  product_id: number;
+  name: number;
+  price: number;
   order_id: number;
-  quantity: number;
-  user_id: number;
 }
 export class mapping {
   async join(): Promise<fetching[]> {
     try {
       const conn = await client.connect();
       const sql =
-        'SELECT products.id as product_id, orders.quantity as quantity, order_id FROM products,orders INNER JOIN mapping ON orders.product_id=mapping.product_id;';
+        'SELECT name, price, order_id FROM products INNER JOIN order_products ON products.id=order_products.product_id;';
       const result = await conn.query(sql);
       conn.release();
       return result.rows;
@@ -22,14 +21,14 @@ export class mapping {
     }
   }
 
-  async top5(): Promise<fetching> {
+  async top5(): Promise<fetching[]> {
     try {
       const conn = await client.connect();
       const sql =
         'SELECT name FROM products INNER JOIN mapping ON products.id=mapping.product_id ORDER BY quantity DESC LIMIT 5;';
       const result = await conn.query(sql);
       conn.release();
-      return result.rows[0];
+      return result.rows;
     } catch (err) {
       throw new Error(`Error  ${err}`);
     }

7cfa86f25735164701ee683afbc298a419147809
M0stafa123 <jodat.ahmed135@gmail.com>
2023-01-13 19:18:28 +0200

    aa

---
+++ src/models/test/models/productSpec.ts
@@ -0,0 +1,36 @@
+import { PRODUCT } from '../../product';
+
+const product = new PRODUCT();
+
+describe('product model methods', () => {
+  it('should contain index method', () => {
+    expect(product.index).toBeDefined();
+  });
+  it('should contain show method', () => {
+    expect(product.show).toBeDefined();
+  });
+  it('should contain create method', () => {
+    expect(product.create).toBeDefined();
+  });
+  it('should contain delete method', () => {
+    expect(product.delete).toBeDefined();
+  });
+  it('should create prodcuct', async () => {
+    const test = await product.create({
+      name: 'product1',
+      price: 5
+    });
+
+    expect(test.id).toEqual(jasmine.any(Number));
+  });
+  it('should search product', async () => {
+    const test = await product.create({
+      id: 10,
+      name: 'product1',
+      price: 10
+    });
+
+    const search = await product.show(test.id as number);
+    expect(search.price).toBe(jasmine.any(Number));
+  });
+});
